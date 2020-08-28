2bf8871da54965914faa50dbd54025459e93f18b
Matheus <matheus.hsb18@outlook.com>
2020-08-28 14:42:45 -0300

    Implementando a listagem dos artigos.

--- src/main/java/com/spring/devBlog/utils/DummyData.java
+++ src/main/java/com/spring/devBlog/utils/DummyData.java
@@ -24,13 +24,13 @@ public class DummyData {
 
 	        List<Artigo> artigoList = new ArrayList<>();
 	        Artigo artigo1 = new Artigo();
-	        artigo1.setAutor("Bruno Alexandre");
+	        artigo1.setAutor("Matheus Henrique");
 	        artigo1.setData(LocalDate.now());
 	        artigo1.setTitulo("Docker");
 	        artigo1.setTexto("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
 
 	        Artigo artigo2 = new Artigo();
-	        artigo2.setAutor("Michelli Brito");
+	        artigo2.setAutor("Matheus Henrique");
 	        artigo2.setData(LocalDate.now());
 	        artigo2.setTitulo("API REST");
 	        artigo2.setTexto("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
