f9df94f43095653637db69b72a77a80eae674c21
Matheus <matheus.hsb18@outlook.com>
2020-08-29 16:13:56 -0300

    Implementando summernote para publicação dos textos e o spring security para bloquear o acesso livre a página de criação de artigos.

--- src/main/java/com/spring/devBlog/controller/ArtigoController.java
+++ src/main/java/com/spring/devBlog/controller/ArtigoController.java
@@ -1,10 +1,16 @@
 package com.spring.devBlog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,6 +29,30 @@ public class ArtigoController {
 		List<Artigo> artigos = artigoService.findAll();
 		mv.addObject("artigos", artigos);
 		return mv;
+	}
+	
+	@RequestMapping(value = "/artigos/{id}", method = RequestMethod.GET)
+	public ModelAndView getArtigoDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("artigoDetails");
+		Artigo artigo = artigoService.findById(id);
+		mv.addObject("artigo", artigo);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/newArtigo", method = RequestMethod.GET)
+	public String getArtigoForm() {
+		return "/artigoForm";
+	}
+	
+	@RequestMapping(value = "/newArtigo", method = RequestMethod.POST)
+	public String saveArtigo(@Valid Artigo artigo, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+			return "redirect:/newArtigo";
+		}
+		artigo.setData(LocalDate.now());
+		artigoService.save(artigo);
+		return "redirect:/artigos";
 	}
 
 }
